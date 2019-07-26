00c57c1d80d1a0c9d86f69113ea6fb54ebe20bd5
Raketenlurch <annkatrin.le@gmx.de>
2019-07-26 23:47:49 +0200

    Add social.py and update gitignore

--- Bot.py
+++ Bot.py
@@ -1,5 +1,24 @@
-import requests
-import beautifulsoup
+import encodings.idna
+import helpers
+import social
+import sys
 
-class Bot:
-    
+if __name__ == '__main__':
+    """ It all starts here...
+        This function will get a new Tweet from the configured Twitter Account and publish to the configured Mastodon instance.
+        It will only toot once per invokation to avoid flooding the instance.
+    """
+
+    tweets = social.getTweets()
+
+    if not tweets:
+        helpers._error('__main__ => No tweets fetched.')
+        sys.exit()
+
+    helpers._info('__main__ => ' + str(len(tweets)) + ' tweets fetched.')
+
+    for tweet in tweets:
+        if social.tootTheTweet(tweet):
+            helpers._info('__main__ => Tooted ' + tweet['text'] + '"')
+            helpers._info('__main__ => Tooting less is tooting more. Sleeping...')
+            sys.exit()
